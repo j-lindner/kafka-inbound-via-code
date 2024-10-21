39e064e713590a470383f0617a2d8c827a556011
jens lindner <jens.lindner@camunda.com>
2024-10-21 14:49:07 +0200

    working example

---
+++ src/main/java/com/camunda/consulting/kafkainboundviacode/KafkaInboundViaCodeApplication.java
@@ -0,0 +1,13 @@
+package com.camunda.consulting.kafkainboundviacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaInboundViaCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaInboundViaCodeApplication.class, args);
+    }
+
+}
